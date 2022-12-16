15554db23aa08dd35090bac1e8fe327508bea94f
Caíque Santos <caiquesantos04087@Gmail.com>
2022-12-16 17:15:30 -0300

    att 2

--- projeto004/script.js
+++ projeto004/script.js
@@ -30,23 +30,28 @@ function on_off() {
 
 function resultado() {
   let resultado = document.getElementById("res").innerHTML;
-  resposta = eval(resultado);
-  if (ligado == true) {
-    if (res.innerHTML.length == 0) {
-      alert("INFORME VALORES PARA CONTINUAR!!!");
-      res.innerHTML = "";
-    } else {
-      if (isNaN(resposta)) {
-        alert("ERRO!!! 0 NÃO PODE SER DIVIDIDO POR 0");
-        limpar();
-      } else if (isFinite(resposta)) {
-        res.innerHTML = resposta;
+  try {
+    resposta = eval(resultado);
+    if (ligado == true) {
+      if (res.innerHTML.length == 0) {
+        alert("INFORME VALORES PARA CONTINUAR!!!");
+        res.innerHTML = "";
       } else {
-        alert("ERRO!!! NÃO A COMO FAZER ESSA DIVISÃO POR 0!!!");
-        limpar();
+        if (isNaN(resposta)) {
+          alert("ERRO!!! 0 NÃO PODE SER DIVIDIDO POR 0");
+          limpar();
+        } else if (isFinite(resposta)) {
+          res.innerHTML = resposta;
+        } else {
+          alert("ERRO!!! NÃO A COMO FAZER ESSA DIVISÃO POR 0!!!");
+          limpar();
+        }
       }
+    } else {
+      alert("A CALCULADORA ESTÁ DESLIGADA!!");
     }
-  } else {
-    alert("A CALCULADORA ESTÁ DESLIGADA!!");
+  } catch (e) {
+    alert("ERRO!!! INFORME UMA OPERAÇÃO VÁLIDA");
+    limpar();
   }
 }
